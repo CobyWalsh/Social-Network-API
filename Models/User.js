5f7e1c11b18690f992d60f57e781d8b53b4a444a
Coby Walsh <cobywalsh7@yahoo.com>
2023-08-24 18:26:04 -0600

    working on user.js model

--- Models/User.js
+++ Models/User.js
@@ -1,1 +1,31 @@
-const mongoose = require('mongoose');
+const {Schema, model} = require("mongoose");
+const thoughtsSchema = require('./Thoughts');
+
+// The userNameSchema defines the schema of the subdocument
+const userSchema = new Schema(
+  { 
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    }
+  }
+);
+
+const emailSchema = new mongoose.Schema({
+  title: { type: String, required: true, match: /.+\@.+\..+/, unique: true },
+});
+await emailSchema.create([
+  { email: "gmail@google.com" },
+  { email: "bill@microsoft.com" },
+  { email: "test@gmail.com" },
+]);
+await emailSchema.init();
+try {
+  await emailSchema.create({ email: "gmail@google.com" });
+} catch (error) {
+  error.message; 
+}
+thoughts: [thoughtsSchema]
+
