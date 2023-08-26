c1082c465bb3f28f6f6318010b99aecd0f6f05ab
Coby Walsh <cobywalsh7@yahoo.com>
2023-08-26 13:11:10 -0600

    added userRoutes.js file to api routes foulder

---
+++ Routes/api/userRoutes.js
@@ -0,0 +1,14 @@
+// userRoutes.js
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController');
+
+// Existing user routes...
+
+// POST to add a new friend
+router.post('/api/users/:userId/friends/:friendId', UserController.addFriend);
+
+// DELETE to remove a friend
+router.delete('/api/users/:userId/friends/:friendId', UserController.removeFriend);
+
+module.exports = router;
